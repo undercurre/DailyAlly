8c64a98dd926fe5cdc422eddbf3e93fdc351e09a
undercurre <undercurre@163.com>
2023-11-22 02:43:36 +0800

    feat: 列表页示例

--- views/Task.tsx
+++ views/Task.tsx
@@ -1,43 +1,64 @@
 import React from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-const Tasks = () => {
-  // 示例数据
-  const data = [
-    {id: '1', title: 'Item 1'},
-    {id: '2', title: 'Item 2'},
-    {id: '3', title: 'Item 3'},
-    // ... 可以添加更多的数据项
-  ];
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-  // 渲染每一项的函数
-  const renderItem = (item: {id: string; title: String}) => (
-    <View style={styles.cell}>
-      <Text>{item.title}</Text>
+const Item = ({title}) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
     </View>
   );
+};
+
+const TaskScreen = () => {
+  const renderItem = ({item}) => <Item title={item.title} />;
 
   return (
-    <View style={styles.container}>
-      {/* <FlatList
-        data={data}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
-      /> */}
-    </View>
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  cell: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
-export default Tasks;
+export default TaskScreen;
